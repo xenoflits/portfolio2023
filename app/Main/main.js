3e96e97df84490203173b1e4844130f757c7737d
soeradj <soeradj.mahabiersing@gmail.com>
2023-05-25 21:44:56 +0200

    main af, met een placeholder image

--- app/Main/main.js
+++ app/Main/main.js
@@ -1,9 +1,28 @@
-import styles from './main.module.css'
+import styles from './main.module.css';
+import Image from 'next/image';
+import halfface from './halfface.jpg'
 
 const Main = () => {
-    return(
-        <div>main</div>
-    )
-}
+    return (
+        <div className={styles.main}>
+            <div className={styles.mainleft}>
+                <div className={styles.mainleftcontent}>
+                <h1 className={styles.mainheader}>
+                    Hey, I'm <span className={styles.mainheaderred}>Soeradj</span>
+                </h1>
+                <h2 className={styles.secondheader}>
+                    a <span className={styles.secondheaderred}>self-taught</span> full-stack developer.
+                </h2>
+                <p className={styles.caption}>Stick around to see some of my work</p>
 
-export default Main
+                <button className={styles.button}>See my work</button>
+                </div>
+            </div>
+            <div className={styles.mainright}>
+            <Image className={styles.halfface} src={halfface} alt="halfface"/>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
