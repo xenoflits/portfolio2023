9eebca5891cac7b636436ca3356ffb97bd888100
soeradj <soeradj.mahabiersing@gmail.com>
2023-05-25 22:00:21 +0200

    added some skelleton work on projects

--- app/Projects/projects.js
+++ app/Projects/projects.js
@@ -1,8 +1,41 @@
+"use client"
 import styles from './projects.module.css'
+import { useState } from 'react'
 
 const Projects = () => {
-    return(
-        <div>projects</div>
+    const [projectNumber,setProjectNumber] = useState(0);
+
+    const toggleproject = (type) => {
+        switch (type) {
+            case "plus":
+            setProjectNumber(projectNumber + 1); 
+            break;
+            case "min":
+            if (projectNumber > 0){
+            setProjectNumber(projectNumber - 1);
+            }    
+            break;          }
+    }
+
+
+    return (
+        <div>
+            <div>header</div>
+            <h1>Interesting projects for you to see</h1>
+            <div>
+                <div>buttonrail
+                    <div onClick={()=>toggleproject("min")}>button left</div>
+                    <div>counter: {projectNumber}</div>
+                    <div onClick={()=>toggleproject("plus")}>button right</div>
+                </div>
+                <div>
+                    <div>project title</div>
+                    <div>project description</div>
+                    <div>view project</div>
+                    <div> project image</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
